976681701f2a4d5aaf2653dd0f81fd29985a2943
Detectov <dannymou7426@hotmail.com>
2024-02-06 13:30:44 -0600

    app started

---
+++ HackInterno/HackInterno/HackInternoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackInternoApp.swift
+//  HackInterno
+//
+//  Created by Daniel Moukarzel on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HackInternoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
---
+++ HackInterno/HackInterno/StartView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  HackInterno
+//
+//  Created by Daniel Moukarzel on 05/02/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        NavigationView{
+            ZStack{
+                Color("background1")
+                    .ignoresSafeArea()
+                VStack(){
+                    HStack(){
+                        Text("ESCANEA UN")
+                            .font(.system(size: 40, weight: .bold))
+                    }
+                    
+                    
+                    
+                        
+                    
+                }
+                
+            }
+        }
+        
+    }
+}
+
