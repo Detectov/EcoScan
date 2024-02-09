1bd7c79096a819a5bb8f34fd055b4ecef985ca07
Feintt <0253214@up.edu.mx>
2024-02-09 17:33:38 -0600

    added auth for jwt

--- EcoApi/lib/eco_api_web/router.ex
+++ EcoApi/lib/eco_api_web/router.ex
@@ -5,11 +5,22 @@ defmodule EcoApiWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :api_auth do
+    plug Guardian.Plug.VerifyHeader, scheme: "Bearer", module: EcoApi.Guardian
+    plug Guardian.Plug.LoadResource, ensure: true, module: EcoApi.Guardian
+  end
+
   scope "/api", EcoApiWeb do
     pipe_through :api
 
     post "/users", UserController, :create
     post "/login", SessionController, :create
-    # Otros endpoints que no requieren autenticación previa
+  end
+
+  scope "/api", EcoApiWeb do
+    # Usa el pipeline de autenticación aquí
+    pipe_through [:api, :api_auth]
+
+    get "/hello", DefaultController, :index
   end
 end
